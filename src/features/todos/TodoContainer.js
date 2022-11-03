3790eca864bf4717b312184e95101a038d1c32a4
Donte Ladatto <donte279@gmail.com>
2022-11-03 15:56:08 -0500

    commit tree c7f40bef7655a8a9f3e150858be8172d5420b92b

--- src/features/todos/TodoContainer.js
+++ src/features/todos/TodoContainer.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { useSelector } from "react-redux"
+import Todo from "./Todo"
 
 function TodoContainer() {
-  return <div>TodoContainer</div>;
+  const todos = useSelector(state => state.todos.entities)
+
+  const displayTodos = todos.map((t, i) => {
+    return (
+    <Todo key={i} text={t} />
+    )
+  })
+
+  return <ul>{displayTodos}</ul>;
 }
 
 export default TodoContainer;
